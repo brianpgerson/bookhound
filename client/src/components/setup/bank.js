91d4d831fa5e4b38880cb9b9a8389bf32c63a9b0
Brian Gerson <brianpgerson@gmail.com>
2017-07-02 15:59:11 -0700

    Plaid link update

--- client/src/components/setup/bank.js
+++ client/src/components/setup/bank.js
@@ -28,33 +28,33 @@ class Bank extends Component {
   renderLink() {
     return this.props.plaidPublicKey ?
       (<PlaidLink publicKey={this.props.plaidPublicKey}
-                product="auth"
-                env="sandbox"
-                apiVersion="v2"
-                selectAccount={true}
-                clientName="bookhound"
-                className="btn btn-primary"
-                style={{}}
-                onSuccess={this.handleOnSuccess.bind(this)}/>) :
-      (<button className="btn-primary btn">Open Link</button>);
+                  product='auth'
+                  env='sandbox'
+                  apiVersion='v2'
+                  selectAccount={true}
+                  clientName='bookhound'
+                  className='btn btn-primary'
+                  style={{}}
+                  onSuccess={this.handleOnSuccess.bind(this)}/>) :
+      (<button className='btn-primary btn'>Open Link</button>);
    }
 
   render() {
     const { bank } = this.props;
     return (
       <div>
-        <h1 className="text-center">{bank ? 'Update Your Bank' : 'Connect Your Bank'}</h1>
-        <section className="container">
-          <div className="row">
-            <div className="col-md-4 col-md-offset-4 is-white-background form-panel">
-              <div className="row">
+        <h1 className='text-center'>{bank ? 'Update Your Bank' : 'Connect Your Bank'}</h1>
+        <section className='container'>
+          <div className='row'>
+            <div className='col-md-4 col-md-offset-4 is-white-background form-panel'>
+              <div className='row'>
                 <div>
                   bookhound uses Plaid to securely and conveniently connect to bank accounts without the need for microdeposits.
                 </div>
               </div>
               <hr />
               {this.renderAlert()}
-              <div className="row">
+              <div className='row'>
                 {this.renderLink()}
               </div>
             </div>
