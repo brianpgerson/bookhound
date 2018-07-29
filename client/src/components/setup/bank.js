8ff1ff88a567b5b13d960459b73e6f80c7791ab5
Brian Gerson <brianpgerson@gmail.com>
2018-07-29 15:54:50 -0700

    Use dev

--- client/src/components/setup/bank.js
+++ client/src/components/setup/bank.js
@@ -30,7 +30,7 @@ class Bank extends Component {
     return this.props.plaidPublicKey ?
       (<PlaidLink publicKey={this.props.plaidPublicKey}
                   product='auth'
-                  env='sandbox'
+                  env='development'
                   apiVersion='v2'
                   selectAccount={true}
                   clientName='bookhound'
