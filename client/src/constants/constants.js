5ee2d8c4cdca3841f232849672e195d67e20f2b1
Brian Gerson <brianpgerson@gmail.com>
2019-01-13 19:01:41 -0800

    another thing

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,5 +1,5 @@
 export const CLIENT_ROOT_URL = 'http://localhost:8080';
-export const API_URL = 'https://www.bookhound.co/api';
+export const API_URL = 'http://www.bookhound.co/api';
 
 export const states = [
     {
